a2bbbdc31815274dc5f21edf1beb33878ec54d6c
unknown <you@example.com>
2022-09-02 16:41:07 -0300

    first commit

---
+++ src/paginas/detalhes/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {View,Text} from 'react-native'
+
+
+
+
+export default function Detalhes(){
+
+return(
+
+    <View>
+
+    <Text> Páginas Detalhes </Text>
+
+    </View>
+
+
+);
+
+
+}
--- src/paginas/home/index.js
+++ src/paginas/home/index.js
@@ -1,22 +1,32 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react'
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 
 
 export default function Home(){
 
+const navigation = useNavigation();
+
+function paginaDetalhes(){
+navigation.navigate('Detalhes')
+
+}
+
+
 return(
 
     <View>
 
-    <Text> ESSA É PAGINA INICIAL </Text>
+    <Text> Essa é a página inicial </Text>
+
+    <Button title = 'Ir para detalhes' onPress = {paginaDetalhes}/>
 
     </View>
 
 
 );
-
-
 }
+
